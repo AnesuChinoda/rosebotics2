f4f98b3780e58eaa73b25cd8771f80250dbd692a
polsteaj <polsteaj@rose-hulman.edu>
2018-11-05 22:12:57 -0500

    wrote the arm and claw class

--- src/rosebotics_new.py
+++ src/rosebotics_new.py
@@ -688,9 +688,9 @@ class ArmAndClaw(object):
     """
     A class for the arm and its associated claw.
     Primary authors:  The ev3dev authors, David Mutchler, Dave Fisher,
-    their colleagues, the entire team, and PUT_YOUR_NAME_HERE.
+    their colleagues, the entire team, and Alec Polster.
     """
-    # TODO: In the above line, put the name of the primary author of this class.
+    # DONE: In the above line, put the name of the primary author of this class.
 
     def __init__(self, touch_sensor, port=ev3.OUTPUT_A):
         # The ArmAndClaw's  motor  is not really a Wheel, of course,
@@ -712,7 +712,17 @@ class ArmAndClaw(object):
         again at a reasonable speed. Then set the motor's position to 0.
         (Hence, 0 means all the way DOWN and 14.2 * 360 means all the way UP).
         """
-        # TODO: Do this as STEP 2 of implementing this class.
+        # DONE: Do this as STEP 2 of implementing this class.
+        self.motor.start_spinning(100)
+        while True:
+            if self.touch_sensor.is_pressed() is True:
+                break
+        self.motor.start_spinning(-100)
+        while True:
+            if self.motor.get_degrees_spun() >= (14.2 * 360):
+                break
+        self.motor.stop_spinning(100)
+        self.motor.reset_degrees_spun(0)
 
     def raise_arm_and_close_claw(self):
         """
@@ -721,11 +731,22 @@ class ArmAndClaw(object):
         Positive speeds make the arm go UP; negative speeds make it go DOWN.
         Stop when the touch sensor is pressed.
         """
-        # TODO: Do this as STEP 1 of implementing this class.
+        # DONE: Do this as STEP 1 of implementing this class.
+        self.motor.start_spinning(100)
+        while True:
+            if self.touch_sensor.is_pressed() is True:
+                break
+        self.motor.stop_spinning(100)
 
     def move_arm_to_position(self, position):
         """
         Spin the arm's motor until it reaches the given position.
         Move at a reasonable speed.
         """
-        # TODO: Do this as STEP 3 of implementing this class.
+        # DONE: Do this as STEP 3 of implementing this class.
+        self.motor.start_spinning(100)
+        while True:
+            if self.motor.get_degrees_spun() == position:
+                self.motor.stop_spinning(100)
+                break
+
