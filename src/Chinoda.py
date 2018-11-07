5ff8f986f7adc4cf2883cb88ccf25fe082425727
chinoda <chinoad@rose-hulman.edu>
2018-11-07 01:21:03 -0500

    robot beep

--- src/Chinoda.py
+++ src/Chinoda.py
@@ -4,13 +4,14 @@
 """
 
 import rosebotics_new as rb
+import ev3dev.ev3 as ev3
 
 
 def main():
     """ Runs YOUR specific part of the project """
     robot = rb.Snatch3rRobot()
-    polyogon(robot, 4, 5)
-
+    #polyogon(robot, 4, 5)
+    follow_black_line(robot)
 def polyogon(robot, n, inches):
     for k in range(n):
         robot.drive_system.go_straight_inches(inches)
@@ -27,12 +28,15 @@ def follow_black_line(robot):
                 robot.drive_system.start_moving(50, 50)
 
 
-def go_to_color(color):
+def go_to_color(robot, color):
     robot.drive_system.start_moving()
     while True:
         if robot.color_sensor.get_color() == color:
             robot.drive_system.stop_moving()
             break
 
+def beep_when_in_front_of_camera(robot):
+    if robot.camera.get_biggest_blob().get_area > 1000:
+        ev3.Sound.beep().wait()
 
 main()
