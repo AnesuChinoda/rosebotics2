fcd83783423a9735c8400d09fa99d9e240bf898e
polsteaj <polsteaj@rose-hulman.edu>
2018-11-12 20:28:07 -0500

    completed project

--- src/Polster.py
+++ src/Polster.py
@@ -16,16 +16,14 @@ def main():
     rc = RemoteControlEtc(robot)
     mqtt_client = com.MqttClient(rc)
     mqtt_client.connect_to_pc()
+    wait_for_ball(robot)
     hike_the_ball(robot)
     while True:
-        if robot.color_sensor.blue() > 70:
-            print('blue', robot.color_sensor.blue())
+        if robot.color_sensor.color == 2:
             run_off_blocker(robot)
-        elif robot.color_sensor.red() > 70:
-            print('red', robot.color_sensor.red())
+        elif robot.color_sensor.color == 5:
             spin_defender(robot)
-        elif robot.color_sensor.green() > 70:
-            print('green', robot.color_sensor.green())
+        elif robot.color_sensor.color == 3:
             celebrate(robot)
             break
     robot.drive_system.stop_moving()
@@ -35,7 +33,6 @@ def hike_the_ball(robot):
     robot.arm.motor.start_spinning(100)
     time.sleep(5)
     robot.arm.motor.stop_spinning('brake')
-    robot.drive_system.start_moving(50, 50)
 
 
 def run_off_blocker(robot):
@@ -53,19 +50,34 @@ def celebrate(robot):
     ev3.Sound.speak('Touchdown, touchdown, touchdown')
     robot.arm.raise_arm_and_close_claw()
     robot.drive_system.right_wheel.start_spinning(100)
-    robot.drive_system.right_wheel.start_spinning(-100)
+    robot.drive_system.left_wheel.start_spinning(-100)
     time.sleep(6)
-    robot.drive_system.right_wheel.start_spinning(50)
-    robot.drive_system.right_wheel.start_spinning(50)
-
+    robot.drive_system.right_wheel.stop_spinning()
+    robot.drive_system.left_wheel.stop_spinning()
+    ev3.Sound.speak('Bengals win')
 
 
 def spin_defender(robot):
     robot.drive_system.right_wheel.start_spinning(100)
-    robot.drive_system.right_wheel.start_spinning(-100)
+    robot.drive_system.left_wheel.start_spinning(-100)
     time.sleep(6)
     robot.drive_system.right_wheel.start_spinning(50)
-    robot.drive_system.right_wheel.start_spinning(50)
+    robot.drive_system.left_wheel.start_spinning(50)
+    time.sleep(5)
+
+
+def wait_for_ball(robot):
+    """
+         Stores a robot.
+            :type robot: rb.Snatch3rRobot
+
+    """
+    while True:
+        if robot.proximity_sensor.get_distance_to_nearest_object() < 10:
+            break
+        time.sleep(3)
+        ev3.Sound.speak('Give me the ball')
+    ev3.Sound.speak('Ball in place, ready for play call')
 
 
 class RemoteControlEtc(object):
@@ -78,19 +90,32 @@ class RemoteControlEtc(object):
         self.robot = robot
 
     def call_play(self, play_string, speed):
-        ev3.Sound.speak(play_string)
-        self.robot.drive_system.start_moving(speed, speed)
+        ev3.Sound.speak(play_string + 'Hut, hut')
+        speed1= int(speed)
+        self.robot.drive_system.start_moving(speed1, speed1)
+
+    def turn_right(self):
+        self.robot.drive_system.right_wheel.stop_spinning()
+        self.robot.drive_system.left_wheel.start_spinning(50)
+        time.sleep(.5)
+        self.robot.drive_system.right_wheel.start_spinning(50)
+
+    def turn_left(self):
+        self.robot.drive_system.left_wheel.stop_spinning()
+        self.robot.drive_system.right_wheel.start_spinning(50)
+        time.sleep(.5)
+        self.robot.drive_system.left_wheel.start_spinning(50)
 
 
-def follow_black_lines(robot):
-    robot.drive_system.start_moving(50, 50)
-    while True:
-        if robot.color_sensor.get_reflected_intensity() > 50:
-            robot.drive_system.left_wheel.start_spinning(20)
-            print(robot.color_sensor.get_reflected_intensity())
-        else:
-            robot.drive_system.left_wheel.start_spinning(50)
-            print(robot.color_sensor.get_reflected_intensity())
+# def follow_black_lines(robot):
+#     robot.drive_system.start_moving(50, 50)
+#     while True:
+#         if robot.color_sensor.get_reflected_intensity() > 50:
+#             robot.drive_system.left_wheel.start_spinning(20)
+#             print(robot.color_sensor.get_reflected_intensity())
+#         else:
+#             robot.drive_system.left_wheel.start_spinning(50)
+#             print(robot.color_sensor.get_reflected_intensity())
 
 
 main()
